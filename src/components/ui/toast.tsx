767f0f904e54141c9b81bf98022e6cdc9b8fb76f
Luis Bree <LuisBree@gmail.com>
2025-06-24 13:51:54 +0000

    Sigue siendo tapada por el logo. Adjunto captura.

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed bottom-4 left-8 z-[100] flex max-h-screen w-auto max-w-md flex-col-reverse gap-2",
+      "fixed bottom-4 left-16 z-[100] flex max-h-screen w-auto max-w-md flex-col-reverse gap-2",
       className
     )}
     {...props}
